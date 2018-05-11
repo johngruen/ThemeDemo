a249f02b0e2e89025684fd1a6f02ae4fc94a9ba1
John Gruen <john.gruen@gmail.com>
2018-05-11 09:02:12 -0400

    add images and rework keys

--- background.js
+++ background.js
@@ -1,31 +1,52 @@
 const theme = {
   colors: {
-    accentcolor: 'cyan', // USED
-    tab_background_text: 'white',
+    accentcolor: 'red', // USED
     button_background_active: 'darkblue',
     button_background_hover: 'lime',
-    frame_inactive: 'orange', // this one needs the window unfocused
-    icons: 'olive',
+    frame_inactive: 'orange', // this one needs the window unfocused, mdn views this as an alias for accentcolor
+    icons: 'yellow',
     icons_attention: 'red',
     popup: 'red',
-    popup_border: 'green',
-    popup_text: 'blue',
-    popup_highlight: 'pink', // this and the one below don't seem to work. supposed to set selected awesomebar item
+    popup_border: 'cyan',
     popup_highlight_text: 'green',
-    tab_line: 'navy', //USED
+    popup_highlight: 'pink', // this and the one below don't seem to work. supposed to set selected awesomebar item
+    popup_text: 'blue',
+    tab_background_text: 'white',
+    tab_line: 'orange', //USED
     tab_loading: 'salmon',
-    tab_selected: 'green', // i can't make this one work, or it's being overidden somewhere?
+    tab_selected: 'transparent',
     tab_text: 'red',
-    textcolor: 'green', // USED
-    toolbar: 'yellow', // USED
+    textcolor: 'black', // USED
+    toolbar: 'rgba(0, 255, 0, .2)', // USED
     toolbar_bottom_separator: 'lime',
-    toolbar_field_border: 'lime',
-    toolbar_field_separator: 'navy', // this is just the little vertical line that sometimes is in the awesomebar
     toolbar_field: 'pink', // USED
-    toolbar_text: 'black', // USED
+    toolbar_field_border: 'lime',
+    toolbar_field_border_focus: 'red',
+    toolbar_field_focus: 'purple',
+    toolbar_field_separator: 'navy', // this is just the little vertical line that sometimes is in the awesomebar
     toolbar_field_text: 'orange', // USED
+    toolbar_text: 'black', // USED
     toolbar_top_separator: 'salmon',
-    toolbar_vertical_separator: 'blue'
+    toolbar_vertical_separator: 'pink'
+  },
+  images: {
+    additional_backgrounds: [
+      'images/left-gradient.svg',
+      'images/triangle.svg',
+      'images/right-gradient.svg'
+    ]
+  },
+  properties: {
+    additional_backgrounds_alignment: [
+      'left top',
+      'center top',
+      'right top'
+    ],
+    additional_backgrounds_tiling: [
+      'repeat-y',
+      'repeat',
+      'repeat-y'
+    ]
   }
 };
 
